e401159ed4f649347ac8a4a350ef019dfe6c38d0
AliCelikay <alicelikay129@gmail.com>
2022-12-26 17:00:02 -0600

    Added routes, components, pages, and images

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import {Route, Routes} from 'react-router-dom';
+// a box is like a div with style but is from material
+import { Box } from '@mui/material';
+
+import './App.css';
+
+import ExerciseDetail from './components/ExerciseDetail';
+import Home from './pages/home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 export const App = () => {
   return (
-    <div>App</div>
+    <Box width='400px'>
+        <Navbar/>
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/exercise/:id" element={<ExerciseDetail/>} />
+        </Routes>
+        <Footer/>
+    </Box>
   )
 }
 
