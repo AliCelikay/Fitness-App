76653d5e241832abe43dbfb18c555191a8f29a5b
AliCelikay <alicelikay129@gmail.com>
2022-12-27 10:22:40 -0600

    Added home components

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react';
+import {Box} from '@mui/material';
+
+import HeroBanner from '../components/HeroBanner';
+import SearchExercises from '../components/SearchExercises';
+import Exercises from '../components/Exercises';
 
 const home = () => {
   return (
-    <div>home</div>
+    <Box>
+        <HeroBanner/>
+        <SearchExercises/>
+        <Exercises/>
+    </Box>
   )
 }
 
