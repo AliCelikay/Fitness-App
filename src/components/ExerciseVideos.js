9127639ad02cdc5f3aee6f598525069e07333538
AliCelikay <alicelikay129@gmail.com>
2022-12-29 15:07:08 -0600

    fixed video loading bug

--- src/components/ExerciseVideos.js
+++ src/components/ExerciseVideos.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import { Typography, Box, Stack } from '@mui/material';
 
-const ExerciseVideos = () => {
-  return (
-    <div>ExerciseVideos</div>
-  )
+const ExerciseVideos = ({ exerciseVideos, name }) => {
+    //this console.log is necessary because the api needs to make 3 call, the firs 2 which return empty and the 3rd array has data, w/o this console the code for videos will look into an empty array and return error
+    console.log(exerciseVideos)
+    
+    if(!exerciseVideos.length){
+        return 'Loading...'
+    }
+    return (
+        <Box sx={{ marginTop: { lg: '200px', xs: '20px' } }} p="20px">
+            <Typography variant="h3" mb="33px">
+                Watch <span style={{ color: '#ff2625', textTransform: 'capitalize' }}>{name}</span> exercise videos
+            </Typography>
+            <Stack justifyContent="flex-start" flexWrap="wrap" alignItems="center" sx={{ flexDirection: { lg: 'row' }, gap: { lg: '110px', xs: '0' } }}>
+                {/* how many videos do we want to show */}
+                {exerciseVideos?.slice(3, 6).map((item, index) => (
+                    <a
+                        key={index}
+                        className="exercise-video"
+                        href={`https://www.youtube.com/watch?v=${item.video.videoId}`}
+                        target="_blank"
+                        // recommended with _blank
+                        rel="noreferrer"
+                    >
+                        <img src={item.video.thumbnails[0].url} alt={item.video.title} />
+                    </a>
+                ))}
+
+            </Stack>
+        </Box>
+    )
 }
 
 export default ExerciseVideos;
--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -2,25 +2,27 @@ import React, {useEffect, useState} from 'react';
 import { useParams } from 'react-router-dom';
 import {Box} from '@mui/material';
 
-import {exerciseOptions, fetchData} from '../utils/fetchData';
+import {exerciseOptions, fetchData, youtubeOptions} from '../utils/fetchData';
 import Detail from '../components/Detail';
 import SimilarExercises from '../components/SimilarExercises';
 import ExerciseVideos from '../components/ExerciseVideos';
 
 const ExerciseDetail = () => {
-  const [exerciseDetail, setExerciseDetail] = useState({})
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  const [exerciseVideos, setExerciseVideos] = useState([])
   // give access to the number in url
-  const { id } = useParams()
+  const { id } = useParams();
 
   useEffect(() => {
     const fetchExercisesData = async() => {
       const exerciseDbUrl = `https://exercisedb.p.rapidapi.com`;
       const youtubeSearchUrl = `https://youtube-search-and-download.p.rapidapi.com`;
 
-
-      const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/${id}`, exerciseOptions);
-
+      const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`, exerciseOptions);
       setExerciseDetail(exerciseDetailData);
+
+      const exerciseVideosData = await fetchData(`${youtubeSearchUrl}/search?query=${exerciseDetail.name}`, youtubeOptions);
+      setExerciseVideos(exerciseVideosData.contents);
     }
     fetchExercisesData();
     // recalling function whenever id changes
@@ -29,7 +31,7 @@ const ExerciseDetail = () => {
   return (
     <Box>
         <Detail exerciseDetail={exerciseDetail}/>
-        <ExerciseVideos/>
+        <ExerciseVideos exerciseVideos={exerciseVideos} name={exerciseDetail.name}/>
         <SimilarExercises/>
     </Box>
   )
